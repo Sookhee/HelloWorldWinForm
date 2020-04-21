91c5d58e21ff18f8e04e3a84bf8c98d043e0577d
Sookhee <s2018s38@e-mirim.hs.kr>
2020-04-21 14:21:17 +0900

    버튼 클릭 시 메세지 출력

--- HelloWorldWinForm/Form1.cs
+++ HelloWorldWinForm/Form1.cs
@@ -19,7 +19,7 @@ namespace HelloWorldWinForm
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("반갑습니다!", "C# WinForm 첫 프로젝트");
         }
     }
 }
